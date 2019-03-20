9f3e2e0fb438fef5d18f985faec58159e1efd1a0
Erwann Vaissac <erwann.vaissac@iut-rodez.fr>
2019-03-20 16:05:04 +0100

    GIT Fix 4.2 function to get default owner

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -20,3 +20,24 @@ CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
     END
     
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
+    DECLARE
+        defaultOwner"user"%rowtype;
+        nowDate date=now();
+    
+    BEGIN
+        defaultOwner := get_default_owner();
+		return query
+		    update activity
+		    SET owner_id = defaultOwner.id,
+		        modification_date = nowDate
+		    where owner_id is null 
+		    returning *;
+    
+    END
+--1 Chercher les activités sans "Owner";            
+--2 Attribuer à ses activités le "Default owner";    
+--3 Retourner les activités modifiées
+
+$$ LANGUAGE plpgsql;
